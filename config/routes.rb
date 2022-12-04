ea634d1cf7bb50823a020c5b574411b492ec1f9b
Dhiego Dias <ddias003@gmail.com>
2022-12-04 15:13:45 -0300

    creating transactions controller and setting up route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  post 'transactions', to: 'transactions#create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
