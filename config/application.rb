0d80b56530257bad2a6523b9961e66e78dc92199
Dhiego Dias <ddias003@gmail.com>
2022-12-06 20:28:16 -0300

    setting up local time in application

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,9 @@ module DesafioRiskTest
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = "Brasilia"
     # config.eager_load_paths << Rails.root.join("extras")
+    # config.autoloader = :classic
+    config.eager_load_paths << "#{Rails.root}/app/transactions"
   end
 end
