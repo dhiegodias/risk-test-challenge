15d4882f120e64cf6cb63507d220903c7981f0a2
Dhiego Dias <ddias003@gmail.com>
2022-12-06 20:27:01 -0300

    adding create method and use_case to the controller

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,4 +1,19 @@
 class TransactionsController < ApplicationController
   def create
+    use_case = ::Transactions::UseCases::ValidateRisk.build
+    use_case.execute(transaction_hash: transaction_params)
+    render json: { "transaction_id": transaction_id, "recommendation": "approve" }, status: :created
+  rescue ::Transactions::Exceptions::FraudulentTransactionException => exception
+    render json: { "transaction_id": transaction_id, "recommendation": "deny" }, status: :bad_request
+  end
+
+  private
+
+  def transaction_params
+    params.permit(:transaction_id, :merchant_id, :user_id, :card_number, :transaction_date, :transaction_amount, :device_id)
+  end
+
+  def transaction_id
+    transaction_params[:transaction_id]
   end
 end
